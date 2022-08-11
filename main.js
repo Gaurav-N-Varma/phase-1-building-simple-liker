134a9a75280877b512af2c7070574b9f487d30df
Gaurav <gaurav.narayan.varma@gmail.com>
2022-08-11 17:52:27 -0400

    commit tree 67c8db3a8cb0e6db34525d5419c53a46a2b1567e

--- main.js
+++ main.js
@@ -4,8 +4,40 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// Assigning the empty heart elements to a variable
+hearts = document.querySelectorAll('.like-glyph')
 
+// Looping through each heart and calling an event listener
+hearts.forEach((heart) => {
+  // Triggered on click
+  heart.addEventListener('click', () => {
+    // Sending a get request to the server
+    // If a successful promise will execute accordingly, and vice versa
+    mimicServerCall()
+      .then(ifSuccessful)
+      .catch(ifFailure)
+  })
+  
+  // Defining ifSuccessful function
+  function ifSuccessful(){
+    if (heart.textContent === EMPTY_HEART){
+      heart.textContent = FULL_HEART
+      heart.classList.add('activated-heart')
+    } else if (heart.textContent === FULL_HEART){
+      heart.textContent = EMPTY_HEART
+      heart.classList.remove('activated-heart')
+    }
+  }
 
+  // Defining ifFailure function
+  function ifFailure(){
+    errorModal = document.querySelector('#modal')
+    errorModal.className = ''
+    
+    // Setting timeer on error modal
+    setTimeout(() => {errorModal.className = 'hidden'}, 3000)
+  }
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
